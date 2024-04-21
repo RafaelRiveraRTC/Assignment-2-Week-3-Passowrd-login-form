c6fc66c2d0915cb0e9598efa53f4d423d59e519e
RafaelRiveraRTC <Tropicalcone25@gmail.com>
2024-04-20 18:38:24 -0700

    Update: Have posted string userName and passowrd inside of public partial class,a. unsure why it is not working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,11 +18,26 @@ namespace Assignment_2_Week_3_Passowrd_login_form
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+
     public partial class MainWindow : Window
     {
+
+
+       
+            string userName = userNameInfo.Text;
+
+            string password = passwordInfoHere.Password;
+        
+     
+
+  
+
         public MainWindow()
         {
             InitializeComponent();
+
+     
         }
         //------------------------------------------------------------------------------------------------------>
         //These are voids i made accidentally,attempting to remove or comment them out messes with the whole code block so i have elected to keep them here
@@ -36,16 +51,16 @@ namespace Assignment_2_Week_3_Passowrd_login_form
 
         }
         //--------------------------------------------------------------------------------------------------->
-        private void Sign_Up_Here_Click(object sender, RoutedEventArgs e)
+
+
+        public void Sign_Up_Here_Click(object sender, RoutedEventArgs e)
         {
             //String username and password are the name values that represent
             //what ever the user inputs into the respective boxes
             //total info will display both username and password info
-            string userName = userNameInfo.Text;
+           
 
-            string password = passwordInfoHere.Password;
-
-            string totalInfo = $"Thank You for signing in.  Username: {userName} Password: {password}";
+            string totalInfo = $"Thank You for signing up.  Username: {userName} Password: {password}";
 
 
             if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
@@ -53,26 +68,47 @@ namespace Assignment_2_Week_3_Passowrd_login_form
 
                 //if the password box or the username is empty,it will display the following message
                 MessageBox.Show("I'm sorry this isn't enough info availible,please try again");
-                runDisplay.Text = "I'm sorry this isn't enough info availible,please try again";
+                runDisplay.Text = "I'm sorry this isn't enough info,please try again";
+                userNameInfo.Clear();
+                passwordInfoHere.Clear();
+                //.Clear allows the taskbox to remove the typed inputs as soon as you finish submiting the information
             }
             else if(userName.Length < 8 || password.Length < 8)
             {
                 //if the password bpx or username is below the specified length the following message will display
                 MessageBox.Show("This Password or Username is not long enough please try again");
                 runDisplay.Text = "This Password or Username is not long enough please try again";
+                userNameInfo.Clear();
+                passwordInfoHere.Clear();
+                //.Clear allows the taskbox to remove the typed inputs as soon as you finish submiting the information
             }
             else
             {
                 //if it succeds the first 2 checks the input from the user will display inside the rich text box
-                MessageBox.Show($"Thank You for signing in.  Username: {userName} Password: {password}");
+                MessageBox.Show($"Thank You for signing up.  Username: {userName} Password: {password}");
                 runDisplay.Text = totalInfo;
+                userNameInfo.Clear();
+                passwordInfoHere.Clear();
+                //.Clear allows the taskbox to remove the typed inputs as soon as you finish submiting the information
             }
 
         }
 
         private void Sign_In_Here_Click(object sender, RoutedEventArgs e)
         {
-            
+   
+            //if (!confirmPassword || ! passwordInfoHere)
+            //{
+
+            //}
+            //else if ()
+            //{
+
+            //}
+            //else
+            //{
+            MessageBox.Show("Login Was successful, Welcome User,we hope you enjoy your stay");
+            //}
         }
     }
 }
